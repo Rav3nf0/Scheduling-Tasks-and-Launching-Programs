b87e909f168ba8f55324569c47676b28f06127e0
Rav3nfo <93236837+Rav3nf0@users.noreply.github.com>
2022-08-27 02:26:57 +0530

    Update scheduledWCD.py

--- scheduledWCD.py
+++ scheduledWCD.py
@@ -1,3 +1,11 @@
+# Scheduled Web Comic Downloader
+
+# Write a program that checks the websites of several web comics and automatically downloads the images if the comic was updated since the program’s last visit. 
+# Your operating system’s scheduler (Scheduled Tasks on Windows, launchd on macOS, and cron on Linux) can run your Python program once a day.
+# The Python program itself can download the comic and then copy it to your desktop so that it is easy to find.
+# This will free you from having to check the website yourself to see whether it has updated.
+
+
 from fileinput import filename
 import bs4,time,requests,os
 
