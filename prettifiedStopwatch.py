45f3d5c748a78cbdafb7db7e3defe883e20417b3
Rav3nfo <93236837+Rav3nf0@users.noreply.github.com>
2022-08-27 02:25:19 +0530

    Update prettifiedStopwatch.py

--- prettifiedStopwatch.py
+++ prettifiedStopwatch.py
@@ -1,3 +1,21 @@
+# Prettified Stopwatch
+# Expand the stopwatch project from this chapter so that it uses the rjust() and ljust() string methods to “prettify” the output.
+
+# Instead of output such as this:
+
+# Lap #1: 3.56 (3.56)
+# Lap #2: 8.63 (5.07)
+# Lap #3: 17.68 (9.05)
+# Lap #4: 19.11 (1.43)
+
+# . . . the output will look like this:
+
+# Lap # 1:   3.56 (  3.56)
+# Lap # 2:   8.63 (  5.07)
+# Lap # 3:  17.68 (  9.05)
+# Lap # 4:  19.11 (  1.43)
+
+
 import time,pyperclip
 print('Press ENTER to begin. Afterward, press ENTER to "click" the stopwatch. Press Ctrl-C to quit.')
 
